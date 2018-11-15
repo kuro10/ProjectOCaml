b8afbd327475ba849d1ae55a65fa3b131d1995ee
kuro10 <trantronghieu97@gmail.com>
2018-11-15 23:59:33 +0100

    Create module Bgalgo

---
+++ bgalgo.ml
@@ -0,0 +1,152 @@
+open Graph
+
+type label = int * int
+
+type  path = (id * id * label) list  
+
+(*-----------------------------------------------------------------------
+val exist_path : label graph -> id -> id -> bool
+This function verifies if it exists a path from s to d in graph g
+------------------------------------------------------------------------*)
+let exist_path g s d = 
+	if not (node_exists g s) then raise (Graph_error ("Node " ^ s ^ " does not exists in the graph."))
+	else if not (node_exists g d) then raise (Graph_error ("Node " ^ d ^ " does not exists in the graph."))
+	else
+		
+		let rec loop acu s =
+			(*For each node 'id' at which 's' can reach
+			 *Check by recursive if it exist a path from 'id' to 'd' *)			 
+			List.exists (fun (id,_) -> 
+							if List.mem id acu then false
+							else if id=d then true
+							else loop (id::acu) id ) 
+						(out_arcs g s)
+		in loop [] s
+
+(*-----------------------------------------------------------------------
+val find_path : label graph -> id -> id -> path
+This function finds a path having the min cost from s to d in graph g, return an empty list if it does not exist  
+------------------------------------------------------------------------*)
+
+(*This function finds a list which includes all of paths from a to b *)
+let rec list_path g from_a marked b= match from_a with
+	| [] -> assert false
+	| (x,y,l)::tl -> 
+		if y=b then [from_a] 
+		else	
+			let newmarked = y :: marked in
+			let n = List.filter (fun (id,_) -> not (List.mem id newmarked) ) (out_arcs g y) in
+			List.concat ( List.map (fun (id,l) -> list_path g ( (y,id,l)::from_a) newmarked b ) n)
+
+
+(*This function finds only a path if it exits*)
+(*
+let rec a_path g from_a marked b= match from_a with
+	| [] -> assert false 
+	| (x,y,(f,c))::tl -> 
+		(*if the path from_a already reached at b -> ok*)
+		if y=b then from_a 
+		else	
+			(* otherwise, mark y*)
+			let newmarked = y :: marked in
+			(*check all of out_arcs from y and choose all the nodes which are not marked*)
+			let n = List.filter (fun (id,_) -> not (List.mem id newmarked) ) (out_arcs g y) in
+			(*find from these nodes a path to b
+			 *if a path exists, return it 
+			 *else check the next node *)			
+			let rec loop l = match l with 
+				| [] -> []
+				| (id,(f,c)) :: tl -> 
+					match a_path g ( (y,id,(f,c))::from_a) newmarked b with   
+						| [] -> loop tl 
+						| path -> path  
+			in loop n		
+*)
+
+let find_cost path = List.fold_left (fun acu (_,_,(_,cost)) -> acu + cost) 0 path
+
+(*This function returns a correct ordre path from a to b which having the min cost*)
+let find_path_mincost g s d =
+	if not (node_exists g s) then raise (Graph_error ("Node " ^ s ^ " does not exists in the graph."))
+	else if not (node_exists g d) then raise (Graph_error ("Node " ^ d ^ " does not exists in the graph."))
+	else
+		assert (s<>d);
+		(*Reverse the path, easier to verify the result*)
+		let pathlist = list_path g [(s,s,(0,0))] [s]  d in 
+		let rec loop min path = function	
+			| [] -> path
+			| p :: tl -> if find_cost p < min then loop (find_cost p) p tl else loop min path tl
+		in match List.rev (loop 10000 [] pathlist) with
+			| [] -> []
+			| a :: tl -> tl
+
+(*-----------------------------------------------------------------------
+This function returns the value of flow_min from path, this value is used to update the graph
+------------------------------------------------------------------------*)
+let find_flow_min path = List.fold_left (fun min (_,_,(flow,_)) -> if flow < min then flow else min) 10000 path 
+
+
+(*-----------------------------------------------------------------------
+val print_path : (id * id * label) list -> unit
+This function prints the path, helps to check the results (path,flow_min)
+------------------------------------------------------------------------*)
+let print_path path = 
+	Printf.printf "{ ";
+	List.iter (fun (id1,id2,_) -> Printf.printf "(%s,%s) " id1 id2 ) (path);
+	Printf.printf " -> min flow = %d & cost = %d" (find_flow_min path) (find_cost path);
+	Printf.printf " }\n"
+
+(*-----------------------------------------------------------------------
+val update_graph : label graph -> (id * id * label) list -> label graph
+This function is used to update the graph by given a path
+------------------------------------------------------------------------*)
+let update_graph g path = 
+	(* First, we have the flow_min that can be added*)
+	let flow_min = find_flow_min path in
+	(* Then, we update all the values on this path,by increasing the flow (or decreasing the capacity) of these arcs*)
+	let rec update_path g path = match path with
+		| [] -> g
+		| (a,b,(flow,cost))::tl -> 
+			if flow = flow_min
+			then update_path (remove_arc g a b) tl  (*if the flow of an arc is zero, remove it *) 
+			else let newlabel =  ((flow - flow_min),cost) in
+				update_path (update_arc g a b newlabel ) tl (*otherwise, dercrease the flow and keep the cost*)	
+	(* Finally, we update all the reverse arcs from this path*)	
+	in 
+	let rec update_rev_path g path = match path with
+		| [] -> g
+		| (a,b,(flow,cost))::tl -> 
+			match find_arc g b a with 
+				| None -> let newlabel = (flow_min, 0-cost) in 
+					update_rev_path (add_arc g b a newlabel ) tl  (*if this arc does not exists, we add it into the graph with the flow_min and invert the cost *)
+				| Some (x,cost) -> let newlabel = (x + flow_min, cost) in
+					update_rev_path ( update_arc g b a newlabel ) tl (*if it already exists, increase this value by adding flow_min*)
+	in update_rev_path (update_path g path) path
+
+(*-----------------------------------------------------------------------
+val run_BG_algo : label graph -> id -> id -> label graph
+This function helps to apply the Busacker-Gowen algorithm on the given graph
+Return a graph with max flow  & min cost
+Print the values of max flow & min cost
+------------------------------------------------------------------------*)
+let run_BG_algo g s p =
+	(*While it exists a path from s to p, continue algo 
+	 *cpt is used to count the number of loop *)
+	let rec loop g cpt cost debit= 
+		if exist_path g s p then 
+			(*find a path and its flow min*)
+			let path = find_path_mincost g s p in
+			let flow_min = find_flow_min path in 
+			let newcost = cost + (find_cost path)*flow_min in
+			let newdebit =  debit + flow_min in 
+			(*print_path path; *)
+			(*update the graph with this path *)
+			let newg = update_graph g path in 
+			(*Gfile.export (string_of_int cpt) newg;*)
+			loop newg (cpt+1) newcost newdebit
+		else (g,cost,debit) 
+	in let (result,mincost,maxdebit) = loop g 0 0 0 in
+	begin
+		Printf.printf "Result : min cost = %d &  max debit = %d\n" mincost maxdebit;
+		result 
+	end
---
+++ bgalgo.mli
@@ -0,0 +1,27 @@
+open Graph
+
+(*Type of label of an arc, with a couple of flow & cost*)
+type label = int * int
+
+(* Type of a path from a node to another node
+ * A path is represented by a list of (id,id,label) *)
+type path = (id * id * label ) list    
+
+(*exist_path g s d :  indicates if a path from source_node to destination_node exists in graph g.
+ *@raise Graphe_error if s (ou d) is unknown in the graph *)
+val exist_path : label graph -> id -> id -> bool 
+
+(*find path g s d : finds in the graph a path from soucre_node to destination_node having min cost 
+ *@raise Graphe_error if s (ou d) is unknown in the graph *)
+val find_path_mincost : label graph -> id -> id -> path
+
+(*print_path path  : print this path in terminal*)
+val print_path : path -> unit
+
+(*update_graph g path : update the graph when having the value flot_min got by a path *)
+val update_graph : label graph -> path -> label graph
+
+(*run_FF_algo g s d : apply the Busacker-Gowen algorithm for graph g
+ *return a graph with max flow & min cost
+ *and print the value of *)
+val run_BG_algo : label graph -> id -> id -> label graph
